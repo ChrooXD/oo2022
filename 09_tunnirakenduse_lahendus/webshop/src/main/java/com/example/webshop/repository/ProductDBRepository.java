04271402aab64eaee61b7e2a750acaf9e1d3155a
ChrooXD <ctokko@tlu.ee>
2022-04-22 15:55:33 +0300

    10. nädala töö

--- 09_tunnirakenduse_lahendus/webshop/src/main/java/com/example/webshop/repository/ProductDBRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.webshop.repository;
-
-import com.example.webshop.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductDBRepository extends JpaRepository<Product, Long> {
-}
