21c8c0eab853892c96ce5230ca1b9779b06372d9
ChrooXD <ctokko@tlu.ee>
2022-04-29 15:33:24 +0300

    11.nädal proovikt

--- 09_tunnirakenduse_lahendus/webshop/src/main/java/ee/mihkel/webshop/controller/ProductController.java
+++ 09_tunnirakenduse_lahendus/webshop/src/main/java/ee/mihkel/webshop/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ProductController {
 
     @Autowired
@@ -24,8 +25,9 @@ public class ProductController {
     }
 
     @DeleteMapping("products/{index}") // localhost:8080/products/1 DELETE
-    public void deleteProduct(@PathVariable Long index) {
+    public List<Product> deleteProduct(@PathVariable Long index) {
         productRepository.deleteById(index);
+        return productRepository.findAll();
     }
 
     @DeleteMapping("products")  // localhost:8080/products   DELETE
@@ -39,8 +41,17 @@ public class ProductController {
         productRepository.save(product);
     }
 
-    @PutMapping("products") // localhost:8080/products  PUT
-    public void editProduct(@RequestBody Product product) {
-        productRepository.save(product);
+    @PutMapping("products")
+    public void editProduct(@RequestBody Product product) { productRepository.save(product); }
+
+
+    @PostMapping("add-all-products") // localhost:8080/products  PUT
+    public void addAllProducts(@RequestBody List <Product> product) {
+            productRepository.saveAll(product);
     }
 }
+
+
+// Controller + Model + Repository
+// Andmebaasi
+// React suhtleb Springiga
