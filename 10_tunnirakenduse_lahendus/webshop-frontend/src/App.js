760f154b59873b9782ea13f6dde10f060644d289
Christopher Tokko <89923989+ChrooXD@users.noreply.github.com>
2022-05-05 16:19:46 +0300

    Update App.js

--- 10_tunnirakenduse_lahendus/webshop-frontend/src/App.js
+++ 10_tunnirakenduse_lahendus/webshop-frontend/src/App.js
@@ -1,20 +1,27 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Link, Route, Routes } from 'react-router-dom';
-import Navbar from './components/navbar';
-import Home from './pages/Home';
+import Navbar from './components/Navbar';
 import Add from './pages/Add';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
 import Maintain from './pages/Maintain';
-
+import AddQuestion from './pages/AddQuestion';
+import MaintainQuestion from './pages/MaintainQuestion';
+import AnswerQuestion from './pages/AnswerQuestion';
 
 function App() {
   return (
-    <div className='App'>
-      <><Navbar /><Routes>
-      <Route path="" element={ <Home /> } />
-      <Route path="lisa" element={ <Add /> } />
-      <Route path="halda" element={ <Maintain /> } />
-      <Route path="ostukorv" element={<div>Olen OSTUKORV lehel</div>} />
-    </Routes></>
+    <div className="App">
+      <Navbar />
+      <Routes>
+          <Route path="" element={ <Home />} />
+          <Route path="lisa" element={ <Add />} />
+          <Route path="halda" element={ <Maintain />} />
+          <Route path="ostukorv" element={ <Cart />} />
+          <Route path="lisa_kysimus" element={ <AddQuestion />} />
+          <Route path="halda_kysimusi" element={ <MaintainQuestion />} />
+          <Route path="vasta_kysimustele" element={ <AnswerQuestion />} />
+      </Routes>
     </div>
   );
 }
