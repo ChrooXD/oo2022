8bafbd2730bba988f386102372ab82cf42d23382
Christopher Tokko <89923989+ChrooXD@users.noreply.github.com>
2022-05-05 16:23:32 +0300

    Update QuestionController.java

--- 10_tunnirakenduse_lahendus/webshop/src/main/java/ee/mihkel/webshop/controller/QuestionController.java
+++ 10_tunnirakenduse_lahendus/webshop/src/main/java/ee/mihkel/webshop/controller/QuestionController.java
@@ -1,7 +1,7 @@
-package ee.andrus.webshop.controller;
+package ee.mihkel.webshop.controller;
 
-import ee.andrus.webshop.model.Question;
-import ee.andrus.webshop.repository.QuestionRepository;
+import ee.mihkel.webshop.model.Question;
+import ee.mihkel.webshop.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
